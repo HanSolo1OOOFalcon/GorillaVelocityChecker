88c2c1940a8766d4c222d2a8d48e4e4a82a33d35
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-04-25 23:03:54 +0200

    Add project files.

---
+++ BananaOSTesting.cs
@@ -0,0 +1,124 @@
+﻿using GorillaVelocityChecker;
+using System.Text;
+using UnityEngine;
+
+namespace BananaOS.Pages
+{
+    public class ExamplePage : WatchPage
+    {
+        //What will be shown on the main menu if DisplayOnMainMenu is set to true
+        public override string Title => "Example";
+
+        //Enabling will display your page on the main menu if you're nesting pages you should set this to false
+        public override bool DisplayOnMainMenu => true;
+
+        //This method will be ran after the watch is completely setup
+        public override void OnPostModSetup()
+        {
+            //max selection index so the indicator stays on the screen
+            selectionHandler.maxIndex = 11;
+        }
+
+        //What you return is what is drawn to the watch screen the screen will be updated everytime you press a button
+        public override string OnGetScreenContent()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("<color=yellow>==</color> Example <color=yellow>==</color>");
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(0, "Y+"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(1, "Y-"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(2, "X+"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(3, "X-"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(4, "Z+"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(5, "Z-"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(6, "RY+"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(7, "RY-"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(8, "RX+"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(9, "RX-"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(10, "RZ+"));
+            stringBuilder.AppendLine(selectionHandler.GetOriginalBananaOSSelectionText(11, "RZ-"));
+            return stringBuilder.ToString();
+        }
+
+        public override void OnButtonPressed(WatchButtonType buttonType)
+        {
+            switch (buttonType)
+            {
+                case WatchButtonType.Up:
+                    selectionHandler.MoveSelectionUp();
+                    break;
+
+                case WatchButtonType.Down:
+                    selectionHandler.MoveSelectionDown();
+                    break;
+
+                case WatchButtonType.Enter:
+                    if (selectionHandler.currentIndex == 0)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localPosition += new Vector3(0f, 0.1f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 1)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localPosition -= new Vector3(0f, 0.1f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 2)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localPosition += new Vector3(0.1f, 0f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 3)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localPosition -= new Vector3(0.1f, 0f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 4)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localPosition += new Vector3(0f, 0f, 0.1f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 5)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localPosition -= new Vector3(0f, 0f, 0.1f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 6)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localRotation *= Quaternion.Euler(0f, 10f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 7)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localRotation *= Quaternion.Euler(0f, -10f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 8)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localRotation *= Quaternion.Euler(10f, 0f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 9)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localRotation *= Quaternion.Euler(-10f, 0f, 0f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 10)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localRotation *= Quaternion.Euler(0f, 0f, 10f);
+                        return;
+                    }
+                    if (selectionHandler.currentIndex == 11)
+                    {
+                        GorillaVelocityChecker.Plugin.veloObj.transform.localRotation *= Quaternion.Euler(0f, 0f, -10f);
+                        return;
+                    }
+                    break;
+
+                //It is recommended that you keep this unless you're nesting pages if so you should use the SwitchToPage method
+                case WatchButtonType.Back:
+                    ReturnToMainMenu();
+                    break;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+using System.Globalization;
+using TMPro;
+using System.Collections;
+using GorillaLocomotion;
+
+namespace GorillaVelocityChecker
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+    {
+		static string currentVelocity;
+        static float cooldown = 0.5f, lastTime = 0f;
+		public static GameObject veloObj;
+		static TextMeshPro veloText;
+        static Renderer cooliCoolRenderer;
+
+        void OnEnable()
+		{
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+
+		void OnGameInitialized(object sender, EventArgs e) { }
+        void Start()
+		{
+            veloObj = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            veloObj.name = "Velocity Object Self";
+            veloObj.transform.SetParent(GTPlayer.Instance.headCollider.transform, worldPositionStays: false);
+            veloObj.transform.localPosition = new Vector3(-0.6f, 0.6f, 1.6f);
+            veloObj.transform.localRotation = Quaternion.identity;
+            veloObj.transform.localScale = Vector3.one;
+
+            Destroy(veloObj.GetComponent<Collider>());
+
+            cooliCoolRenderer = veloObj.GetComponent<Renderer>();
+            cooliCoolRenderer.material = new Material(Shader.Find("GUI/Text Shader"));
+
+            veloText = veloObj.AddComponent<TextMeshPro>();
+            veloText.alignment = TextAlignmentOptions.Center;
+            veloText.transform.rotation = Quaternion.Euler(0, 180, 0);
+            veloText.font = GorillaTagger.Instance.offlineVRRig.playerText1.font;
+            veloText.fontSize = 1;
+            veloText.text = "0.0";
+            veloText.color = Color.white;
+        }
+
+        void FixedUpdate()
+		{
+            if (lastTime >= cooldown)
+			{
+                Vector3 velocityVector = GorillaTagger.Instance.offlineVRRig.LatestVelocity();
+                float speedMagnitude = velocityVector.magnitude;
+                currentVelocity = speedMagnitude.ToString("F1", CultureInfo.InvariantCulture);
+                lastTime = 0f;
+            }
+
+            veloText.text = $"Position: {veloObj.transform.localPosition.ToString("F1", CultureInfo.InvariantCulture)}\n Rotation: {veloObj.transform.localEulerAngles.ToString("F1", CultureInfo.InvariantCulture)}";
+
+            lastTime += Time.deltaTime;
+            if (veloObj.transform.localRotation != Quaternion.identity) veloObj.transform.localRotation = Quaternion.identity;
+        }
+    }
+}
